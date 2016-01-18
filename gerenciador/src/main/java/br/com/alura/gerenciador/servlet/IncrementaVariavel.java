414aab871e814d847926253440d56ac755941ec9
caiocmsouza <caiocesar.msouza@gmail.com>
2016-01-18 21:04:55 -0200

    arquivos que não subiram

---
+++ gerenciador/src/main/java/br/com/alura/gerenciador/servlet/IncrementaVariavel.java
@@ -0,0 +1,35 @@
+package br.com.alura.gerenciador.servlet;
+
+public class IncrementaVariavel implements Runnable{
+
+private static volatile double total = 1;
+
+public static void main(String[] args) {
+
+new Thread(new IncrementaVariavel()).start();
+
+new Thread(new IncrementaVariavel()).start();
+
+new Thread(new IncrementaVariavel()).start();
+
+new Thread(new IncrementaVariavel()).start();
+
+new Thread(new IncrementaVariavel()).start();
+
+}
+
+@Override
+
+public void run() {
+
+for(int i=0; i<50; i++){
+
+total = total * 1.5;
+
+System.out.println(total);
+
+}
+
+}
+
+}
